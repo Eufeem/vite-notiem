3850cc5750f0c020a659b84f23ebe95164fefb61
Carlos Jesús García Ortega <eufeemgr@gmail.com>
2023-11-20 23:54:03 -0600

    Update rows

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 
 const Content = () => {
   return (
-      <div className="container mt-2 ms-1 me-1">
+      <div className="container mt-2">
         {/* <div className="row">
         <div className="col-md-12">
           <div className="card border-top-3" style={{ width: '100%', borderTopColor: '#042c93' }}>
@@ -21,7 +21,7 @@ const Content = () => {
       </div> */}
 
         <div className="row mt-3 justify-content-center">
-          <div className="col-md-11">
+          <div className="col-md-12">
             <div className="card border-start-3" style={{ width: '100%', borderLeftColor: '#042c93' }}>
               <div className="card-header">
                 Formulario Usuarios
@@ -67,7 +67,51 @@ const Content = () => {
         </div>
 
         <div className="row mt-3 justify-content-center">
-          <div className="col-md-11">
+          <div className="col-md-6">
+            <div className="card border-start-3" style={{ width: '100%', borderLeftColor: '#042c93' }}>
+              <div className="card-header">
+                Formulario Usuarios
+              </div>
+
+              <div className="card-body">
+                <h5 className="card-title">Agregar Usuario</h5>
+
+                <form>
+                  <div className="row mt-3">
+                    <div className="col-md-4 mb-3">
+                      <label htmlFor="name" className="form-label">Nombre</label>
+                      <input type="text" className="form-control" id="name" placeholder="Ingresa tu nombre" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                      <label htmlFor="lastName" className="form-label">Apellido Paterno</label>
+                      <input type="text" className="form-control" id="lastName" placeholder="Ingresa tu apellido paterno" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                      <label htmlFor="email" className="form-label">Email</label>
+                      <input type="email" className="form-control" id="email" placeholder="Ingresa tu email" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                      <label htmlFor="job" className="form-label">Ocupación</label>
+                      <input type="text" className="form-control" id="job" placeholder="Ingresa tu ocupación" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                      <label htmlFor="age" className="form-label">Edad</label>
+                      <input type="email" className="form-control" id="age" placeholder="Ingresa tu edad" />
+                    </div>
+                  </div>
+
+                  <div className="row mt-3 justify-content-center">
+                    <div className="col-md-4 d-grid gap-2">
+                      <button className="btn btn-primary" type="button">Agregar</button>
+                    </div>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-6">
             <div className="card border-start-3" style={{ width: '100%', borderLeftColor: '#042c93' }}>
               <div className="card-header">
                 Formulario Usuarios
@@ -112,6 +156,7 @@ const Content = () => {
           </div>
         </div>
 
+        
       </div>
   )
 }
